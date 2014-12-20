ee8ecdd7c0f71da742aa9ae27240ac8ef7497875
Sonal <soogli@minion.com>
2014-12-20 16:07:59 +0000

    improvin'

--- engine/source/dpJust.cpp
+++ engine/source/dpJust.cpp
@@ -17,5 +17,22 @@ DPJust::dynamic_setup_string(){
 DPJust::initialize(){
 
 }
+/*
+ * Penality function based on number of spaces in the function
+ * version 1 : quite dumb
+ * ver 2 : extra spaces apart from single spaces between words
+ */
+int DPJust::costFunction(int i, int j){
+    return 0;
+}
+
+int DPJust::solveCases(int position){
+
+    if(position == DPJust::lengthOfMessages){
+        return 0;
+    }
+    return -1;
+}
+
 
 
--- engine/source/dpJust.h
+++ engine/source/dpJust.h
@@ -5,6 +5,7 @@
 
 class DPJust final {
     private:
+        int lengthOfMessage;
         string uglyText;
         struct PrettyLines{
             vector<vector<string>> rows;
@@ -12,6 +13,7 @@ class DPJust final {
         }
         void intialize();
         void dynamic_setup_string();
+        int costFunction(int i, int j);
    public:
         DPJust();
         DPJust(string feedText);
